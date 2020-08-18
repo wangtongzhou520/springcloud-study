bd3eefa5e5b1ffa4662582003f8ef1595655c8cc
王统周 <wangtongzhou520@163.com>
2020-08-18 21:29:05 +0800

    更新用户信息

--- system/system-rest/src/main/java/com/springcloud/study/system/rest/controller/UserController.java
+++ system/system-rest/src/main/java/com/springcloud/study/system/rest/controller/UserController.java
@@ -2,12 +2,16 @@ package com.springcloud.study.system.rest.controller;
 
 import com.springcloud.study.common.core.vo.CommonResponse;
 import com.springcloud.study.system.biz.dto.user.SaveUserDTO;
+import com.springcloud.study.system.biz.dto.user.UpdateUserDTO;
 import com.springcloud.study.system.biz.service.user.SysUserService;
 import com.springcloud.study.system.rest.convert.dept.SysUserRequestConvert;
 import com.springcloud.study.system.rest.request.user.SaveUserRequest;
+import com.springcloud.study.system.rest.request.user.UpdateUserRequest;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,10 +36,27 @@ public class UserController {
      * @param saveUserRequest saveUserRequest
      * @return true or false
      */
+    @PostMapping("/save")
+    @ApiOperation("添加用户信息")
     public CommonResponse<?> saveUser(@RequestBody @Validated SaveUserRequest saveUserRequest) {
         SaveUserDTO saveUserDTO =
                 SysUserRequestConvert.INSTANCE.convert(saveUserRequest);
         sysUserService.saveUser(saveUserDTO);
+        return CommonResponse.ok();
+    }
+
+    /**
+     * 更新用户信息
+     *
+     * @param updateUserRequest updateUserRequest
+     * @return true or false
+     */
+    @PostMapping("/update")
+    @ApiOperation("添加用户信息")
+    public CommonResponse<?> updateUser(@RequestBody @Validated UpdateUserRequest updateUserRequest) {
+        UpdateUserDTO updateUserDTO =
+                SysUserRequestConvert.INSTANCE.convert(updateUserRequest);
+        sysUserService.updateUser(updateUserDTO);
         return CommonResponse.ok();
     }
 }
--- system/system-rest/src/main/java/com/springcloud/study/system/rest/convert/dept/SysUserRequestConvert.java
+++ system/system-rest/src/main/java/com/springcloud/study/system/rest/convert/dept/SysUserRequestConvert.java
@@ -1,7 +1,10 @@
 package com.springcloud.study.system.rest.convert.dept;
 
 import com.springcloud.study.system.biz.dto.user.SaveUserDTO;
+import com.springcloud.study.system.biz.dto.user.UpdateUserDTO;
+import com.springcloud.study.system.rest.request.dept.UpdateDeptRequest;
 import com.springcloud.study.system.rest.request.user.SaveUserRequest;
+import com.springcloud.study.system.rest.request.user.UpdateUserRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
@@ -23,4 +26,13 @@ public interface SysUserRequestConvert {
      * @return saveUserDTO
      */
     SaveUserDTO convert(SaveUserRequest saveUserRequest);
+
+    /**
+     * updateUserRequest request updateUserDTO
+     *
+     * @param updateUserRequest updateUserRequest
+     * @return updateUserDTO
+     */
+    UpdateUserDTO convert(UpdateUserRequest updateUserRequest);
+
 }
