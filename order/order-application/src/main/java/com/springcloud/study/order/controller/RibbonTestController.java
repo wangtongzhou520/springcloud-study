6eb0c56000e32ce0f446a0b06f55c768d984336f
王统周 <wangtongzhou520@163.com>
2020-12-20 11:02:45 +0800

    fegin测试

--- order/order-application/src/main/java/com/springcloud/study/order/controller/RibbonTestController.java
+++ order/order-application/src/main/java/com/springcloud/study/order/controller/RibbonTestController.java
@@ -8,8 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * ribbon 测试
+ *
+ * @author wangtongzhou
+ * @since
+ */
 @RestController
-public class TestController {
+public class RibbonTestController {
 
     @Autowired
     private RestTemplate restTemplate;
